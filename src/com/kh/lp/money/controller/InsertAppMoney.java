fc5f9d8dce181e5c3346664d2e2b55a44ad13577
kewon96 <yang0184@naver.com>
2019-12-21 15:39:28 +0900

    감정신청결제어디다가해...?

--- src/com/kh/lp/money/controller/InsertAppMoney.java
+++ src/com/kh/lp/money/controller/InsertAppMoney.java
@@ -7,9 +7,15 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.kh.lp.money.model.service.MoneyService;
+import com.kh.lp.money.model.vo.Money;
+
+import lombok.extern.log4j.Log4j2;
+
 /**
  * Servlet implementation class InsertAppMoney
  */
+@Log4j2
 @WebServlet("/appmoney.in")
 public class InsertAppMoney extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -30,6 +36,21 @@ public class InsertAppMoney extends HttpServlet {
 		String impId = (String) request.getParameter("impId");
 		int memberNo = Integer.parseInt(request.getParameter("memberNo"));
 		String merId = (String) request.getParameter("merId");
+		int amount = Integer.parseInt(request.getParameter("amount")); // 결제금액
+		
+		log.debug(impId);
+		log.debug(memberNo);
+		log.debug(merId);
+		
+		Money m = new Money();
+		m.setImpId(impId);
+		m.setMemberNo(memberNo);
+		m.setMerId(merId);
+		m.setAmount(amount);
+		
+		int result = new MoneyService().insertApp(m);
+		
+		log.debug(result);
 	}
 
 	/**
--- src/com/kh/lp/money/model/dao/MoneyDao.java
+++ src/com/kh/lp/money/model/dao/MoneyDao.java
@@ -1,5 +1,51 @@
 package com.kh.lp.money.model.dao;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+
+import static com.kh.lp.common.JDBCTemplate.*;
+import com.kh.lp.money.model.vo.Money;
+
 public class MoneyDao {
+	Properties prop = new Properties();
+	
+	public MoneyDao() {
+		String fileName = MoneyDao.class.getResource("/sql/money/money-query.properties").getPath();
+		
+		try {
+			prop.load(new FileReader(fileName));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public int insertApp(Connection con, Money m) {
+		PreparedStatement pstmt = null;
+		int result = 0;
+		
+		String query = prop.getProperty("insertApp");
+		
+		try {
+			pstmt = con.prepareStatement(query);
+			pstmt.setInt(1, m.getMemberNo());
+			pstmt.setString(2, m.getMerId());
+			pstmt.setInt(3, m.getAmount());
+			pstmt.setString(4, m.getImpId());
+			
+			result = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			close(pstmt);
+		}
+		
+		return result;
+	}
 
 }
